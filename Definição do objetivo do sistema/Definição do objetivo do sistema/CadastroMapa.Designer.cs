20c954835929f1ce50d24cddaba428cf81003c1b
LuisAugusto11 <luisaugustomarino@gmail.com>
2023-07-27 22:24:16 -0300

    personalizei os botões/2

--- Definição do objetivo do sistema/Definição do objetivo do sistema/CadastroMapa.Designer.cs
+++ Definição do objetivo do sistema/Definição do objetivo do sistema/CadastroMapa.Designer.cs
@@ -54,6 +54,7 @@ namespace Definição_do_objetivo_do_sistema
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label1.Location = new System.Drawing.Point(188, 41);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(273, 31);
@@ -63,7 +64,9 @@ namespace Definição_do_objetivo_do_sistema
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label2.Location = new System.Drawing.Point(12, 101);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(78, 20);
@@ -73,7 +76,9 @@ namespace Definição_do_objetivo_do_sistema
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label3.Location = new System.Drawing.Point(12, 223);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 20);
@@ -83,7 +88,9 @@ namespace Definição_do_objetivo_do_sistema
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label4.Location = new System.Drawing.Point(12, 163);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(51, 20);
@@ -94,6 +101,7 @@ namespace Definição_do_objetivo_do_sistema
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label5.Location = new System.Drawing.Point(190, 295);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(87, 20);
@@ -103,7 +111,9 @@ namespace Definição_do_objetivo_do_sistema
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label6.Location = new System.Drawing.Point(12, 295);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(41, 20);
@@ -113,7 +123,9 @@ namespace Definição_do_objetivo_do_sistema
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label7.Location = new System.Drawing.Point(12, 361);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(176, 20);
@@ -123,7 +135,9 @@ namespace Definição_do_objetivo_do_sistema
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label8.Location = new System.Drawing.Point(12, 429);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(139, 20);
@@ -181,13 +195,15 @@ namespace Definição_do_objetivo_do_sistema
             // 
             // btnContinuar
             // 
+            this.btnContinuar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.btnContinuar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnContinuar.ForeColor = System.Drawing.Color.DarkMagenta;
             this.btnContinuar.Location = new System.Drawing.Point(250, 527);
             this.btnContinuar.Name = "btnContinuar";
             this.btnContinuar.Size = new System.Drawing.Size(185, 36);
             this.btnContinuar.TabIndex = 15;
             this.btnContinuar.Text = "Continuar";
-            this.btnContinuar.UseVisualStyleBackColor = true;
+            this.btnContinuar.UseVisualStyleBackColor = false;
             this.btnContinuar.Click += new System.EventHandler(this.btnContinuar_Click);
             // 
             // txtEstado
@@ -202,6 +218,7 @@ namespace Definição_do_objetivo_do_sistema
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.DarkMagenta;
             this.label9.Location = new System.Drawing.Point(401, 223);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(60, 20);
@@ -211,13 +228,15 @@ namespace Definição_do_objetivo_do_sistema
             // 
             // BtnLimpar
             // 
+            this.BtnLimpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
             this.BtnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.ForeColor = System.Drawing.Color.DarkMagenta;
             this.BtnLimpar.Location = new System.Drawing.Point(16, 486);
             this.BtnLimpar.Name = "BtnLimpar";
             this.BtnLimpar.Size = new System.Drawing.Size(92, 33);
             this.BtnLimpar.TabIndex = 18;
             this.BtnLimpar.Text = "Limpar";
-            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.UseVisualStyleBackColor = false;
             this.BtnLimpar.Click += new System.EventHandler(this.button1_Click);
             // 
             // CadastroMapa
