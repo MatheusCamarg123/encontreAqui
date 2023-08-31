99fe576a507631004b19137368b7380c6871496e
eubiabarboza <beatriz.lbarboza@senacsp.edu.br>
2023-08-31 20:46:23 -0300

    cadastro detalhes certo

---
+++ Definição do objetivo do sistema/Cadastro_detalhes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using MySql.Data.MySqlClient;
+using Definição_do_objetivo_do_sistema.Models;
+
+
+namespace Definição_do_objetivo_do_sistema
+{
+    public partial class Cadastro_detalhes : Form
+    {
+        public Cadastro_detalhes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncontrar_Click(object sender, EventArgs e)
+        {
+         
+
+           
+
+
+        }
+
+        private void Detalhes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cMobiliado_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
